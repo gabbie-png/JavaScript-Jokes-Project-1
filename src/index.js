6bef196a53ec5fa1aa2939d58f6b99b64e2611fa
Gabriella RoseMarino <Gabriellarosemarino@gmail.com>
2021-05-27 12:03:50 -0400

    work in progress

--- src/index.js
+++ src/index.js
@@ -1,18 +1,46 @@
-
-const button = document.querySelector('.container button');
+const resetButton = document.querySelector('#getReset');
+const jokeButton = document.querySelector('#getJoke');
 const jokeText = document.querySelector('.container p');
+const buttonContainer = document.querySelector('.container')
+const EMPTY_HEART = '♡'
+const FULL_HEART = '♥'
+//const data = []
+document.addEventListener('DOMContentLoaded', function(){})
 
-button.addEventListener('click', getJoke);
-
-async function getJoke(){
-  const jokeData = await fetch('https://official-joke-api.appspot.com/jokes/random', {
-    headers: {
+  
+jokeButton.addEventListener('click', function (e) {
+  fetch('https://official-joke-api.appspot.com/jokes/random', {
+    header: {
       'Accept': 'application/json'
     }
-  });
-  const jokeObj = await jokeData.json();
-  console.log(jokeObj)
-  //jokeText.innerHTML = jokeObj.joke;
-}
+
+  })
+    .then(res => res.json())
+    .then(jokeList => renderJoke(jokeList))
+
+  function renderJoke(haha) {
+
+    const jokeSetup = document.createElement('p')
+    jokeSetup.innerText = haha.setup
+
+    const jokePunchline = document.createElement('p')
+    jokePunchline.innerText = haha.punchline
+    jokeText.append(jokeSetup, jokePunchline)
+
+  }
+
+})
+
+resetButton.addEventListener('click', function pageReload(){
+  location.reload()
+})
+
+//this is the like button
+
   
- 
+  //renderPunchline(jokeList)
+  
+
+
+   
+
